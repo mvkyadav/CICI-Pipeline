83c662cea99ecb7ce37f7ee6f787587d6921df63
mvkyadav <vamshikrishnamadas@gmail.com>
2022-07-30 16:43:10 +0530

    Adding Jenkins file

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,14 @@
+pipeline {
+    agent any {
+
+        stages{
+            stage('Build') {
+                steps {
+                    withMaven (maven: 'maven_3_8_0')
+                    sh 'mvn package'
+                }
+            }
+        }
+
+    }
+}
